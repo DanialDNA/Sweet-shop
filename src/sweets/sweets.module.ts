af434ca5df90175539a447601d2d975a89c0ba46
danialdvdvd@gmail.com <mellow.13821382@gmail.com>
2023-02-17 18:35:29 +0330

    import prismaModule to sweetModule

--- src/sweets/sweets.module.ts
+++ src/sweets/sweets.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SweetsService } from './sweets.service';
 import { SweetsController } from './sweets.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [SweetsController],
-  providers: [SweetsService]
+  providers: [SweetsService],
+  imports: [PrismaModule]
 })
 export class SweetsModule {}
