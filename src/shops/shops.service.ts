e4deb12617b704102e795954f684a6bc72e7b963
danialdvdvd@gmail.com <mellow.13821382@gmail.com>
2023-02-18 01:25:19 +0330

    write create and findall shop service

--- src/shops/shops.service.ts
+++ src/shops/shops.service.ts
@@ -1,26 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { errorList } from 'src/errorList';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateShopDto } from './dto/create-shop.dto';
 import { UpdateShopDto } from './dto/update-shop.dto';
 
 @Injectable()
 export class ShopsService {
-  create(createShopDto: CreateShopDto) {
-    return 'This action adds a new shop';
-  }
+    constructor(private prisma: PrismaService) { }
 
-  findAll() {
-    return `This action returns all shops`;
-  }
 
-  findOne(id: number) {
-    return `This action returns a #${id} shop`;
-  }
+    async create(createShopDto: CreateShopDto) {
+        try {
+            const shop = await this.prisma.shop.create({data: createShopDto})        
+            return 
+            
+        } catch (error) {
+            throw new HttpException(errorList.internalServerError, HttpStatus.INTERNAL_SERVER_ERROR)
 
-  update(id: number, updateShopDto: UpdateShopDto) {
-    return `This action updates a #${id} shop`;
-  }
+        }
 
-  remove(id: number) {
-    return `This action removes a #${id} shop`;
-  }
+    }
+
+    async findAll() {
+        try {
+            const shops = await this.prisma.shop.findMany({})
+            return shops
+
+        } catch (error) {
+            throw new HttpException(errorList.internalServerError, HttpStatus.INTERNAL_SERVER_ERROR)
+
+        }
+
+    }
+
+    findOne(id: number) {
+        return `This action returns a #${id} shop`;
+    }
+
+    update(id: number, updateShopDto: UpdateShopDto) {
+        return `This action updates a #${id} shop`;
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} shop`;
+    }
 }
