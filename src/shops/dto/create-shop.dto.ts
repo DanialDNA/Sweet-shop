a82a5622f00cdcd169c19fce0e979d20246417b1
danialdvdvd@gmail.com <mellow.13821382@gmail.com>
2023-02-18 01:25:39 +0330

    write dto and entity for shop resource

--- src/shops/dto/create-shop.dto.ts
+++ src/shops/dto/create-shop.dto.ts
@@ -1,1 +1,29 @@
-export class CreateShopDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsEmail, IsNotEmpty, IsNumber, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
+
+
+interface sellerInterface {
+    connect: { id: number }
+}
+
+export class CreateShopDto {
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    title: string
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    address: string
+
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    phone: number
+
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    seller: sellerInterface
+}
--- src/shops/entities/shop.entity.ts
+++ src/shops/entities/shop.entity.ts
@@ -1,1 +1,32 @@
-export class Shop {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Shop } from "@prisma/client";
+
+export class ShopEntity implements Shop {
+    @ApiProperty()
+    id: number
+  
+    @ApiProperty()
+    title: string
+  
+    @ApiProperty()
+    address:  string
+  
+  
+    @ApiProperty()
+    phone: number
+    
+  
+    @ApiProperty()
+    sellerId: number
+
+
+    @ApiProperty()
+    createdAt: Date
+
+    @ApiProperty()
+    updatedAt: Date
+
+
+
+
+}
