62f17e4731ec516aff83ab54f9af6efbdd7086f0
danialdvdvd@gmail.com <mellow.13821382@gmail.com>
2023-02-18 01:25:01 +0330

    complete the shops controller

--- src/shops/shops.controller.ts
+++ src/shops/shops.controller.ts
@@ -2,33 +2,45 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ShopsService } from './shops.service';
 import { CreateShopDto } from './dto/create-shop.dto';
 import { UpdateShopDto } from './dto/update-shop.dto';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ShopEntity } from './entities/shop.entity';
 
 @Controller('shops')
+@ApiTags("shops")
 export class ShopsController {
-  constructor(private readonly shopsService: ShopsService) {}
+    constructor(private readonly shopsService: ShopsService) { }
 
-  @Post()
-  create(@Body() createShopDto: CreateShopDto) {
-    return this.shopsService.create(createShopDto);
-  }
 
-  @Get()
-  findAll() {
-    return this.shopsService.findAll();
-  }
+    @Post()
+    @ApiCreatedResponse({ type: ShopEntity })
+    create(@Body() createShopDto: CreateShopDto) {
+        return this.shopsService.create(createShopDto);
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.shopsService.findOne(+id);
-  }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateShopDto: UpdateShopDto) {
-    return this.shopsService.update(+id, updateShopDto);
-  }
+    @Get()
+    @ApiOkResponse({ type: ShopEntity, isArray: true })
+    findAll() {
+        return this.shopsService.findAll();
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.shopsService.remove(+id);
-  }
+    @Get(':id')
+    @ApiOkResponse({ type: ShopEntity })
+    findOne(@Param('id') id: string) {
+        return this.shopsService.findOne(+id);
+    }
+
+    @Patch(':id')
+    @ApiOkResponse({ type: ShopEntity })
+
+    update(@Param('id') id: string, @Body() updateShopDto: UpdateShopDto) {
+        return this.shopsService.update(+id, updateShopDto);
+    }
+
+    @Delete(':id')
+    @ApiOkResponse({ type: ShopEntity })
+
+    remove(@Param('id') id: string) {
+        return this.shopsService.remove(+id);
+    }
 }
