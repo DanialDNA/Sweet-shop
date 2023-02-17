c051c9ea472c29af7cba85e76c7b574ded53d5c7
danialdvdvd@gmail.com <mellow.13821382@gmail.com>
2023-02-17 19:56:10 +0330

    add api-respnses to sweets controllers

--- src/sweets/sweets.controller.ts
+++ src/sweets/sweets.controller.ts
@@ -2,7 +2,8 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { SweetsService } from './sweets.service';
 import { CreateSweetDto } from './dto/create-sweet.dto';
 import { UpdateSweetDto } from './dto/update-sweet.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { SweetEntity } from './entities/sweet.entity';
 
 @Controller('sweets')
 @ApiTags('sweets')
@@ -10,26 +11,31 @@ export class SweetsController {
   constructor(private readonly sweetsService: SweetsService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: SweetEntity })
   create(@Body() createSweetDto: CreateSweetDto) {
     return this.sweetsService.create(createSweetDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: SweetEntity, isArray: true })
   findAll() {
     return this.sweetsService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: SweetEntity})
   findOne(@Param('id') id: string) {
     return this.sweetsService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ type: SweetEntity})
   update(@Param('id') id: string, @Body() updateSweetDto: UpdateSweetDto) {
     return this.sweetsService.update(+id, updateSweetDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: SweetEntity})
   remove(@Param('id') id: string) {
     return this.sweetsService.remove(+id);
   }
