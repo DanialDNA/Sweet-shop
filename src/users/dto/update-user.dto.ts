592ca70f2fd4d65296bf1c89bccbda329a4710a6
danialdvdvd@gmail.com <mellow.13821382@gmail.com>
2023-02-20 11:22:03 +0330

    debug update.dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,7 +1,29 @@
-import { PartialType } from '@nestjs/swagger';
+import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsEmail, IsNotEmpty, IsOptional, IsString, Matches, MaxLength, MinLength } from 'class-validator';
+import { errorList } from 'src/errorList';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
 
+
+export class UpdateUserDto {
     
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    firstname: string
+
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    lastname: string
+
+
+    @ApiProperty()
+    @IsEmail({}, errorList.wrongEmail)
+    email: string
+
 }
